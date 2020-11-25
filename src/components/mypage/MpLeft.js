97a41edec19bf0ae70f8d7f2466c61e9871491eb
Jungwon Lee <leejungwon96@gmail.com>
2020-11-25 15:19:55 +0900

    안돼애애ㅐ

--- src/components/mypage/MpLeft.js
+++ src/components/mypage/MpLeft.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/MyPage.css";
+import Nami from "../../images/lolicon.jpg";
 
 const MpLeft = () => {
   return (
@@ -7,9 +8,10 @@ const MpLeft = () => {
       <div className="mp-profile">
         <h1>프로필사진</h1>
         <div className="box-profile">
-          <img className="profile_img" src="" alt="cat" />
+          <img className="profile-img" src={Nami} alt="cat" />
         </div>
       </div>
+
       <div>
         <h1>닉네임</h1>
       </div>
--- src/components/mypage/MpRight.js
+++ src/components/mypage/MpRight.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/MyPage.css";
 
 const MpRight = () => {
+  const [input, setInput] = useState("");
+  const [champ, setChamp] = useState([{ id: 0, name: "" }]);
+  const [nextId, setNextId] = useState(0);
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const OnClickButton = () => {
+    const nextChamp = champ.concat({
+      id: nextId,
+      name: input,
+    });
+
+    setNextId(nextId + 1);
+    setChamp(nextChamp);
+    console.log(champ);
+  };
+  const champList = champ.map((name, index) => (
+    <li key={index}>{champ.name}</li>
+  ));
+
   return (
-    <div>
-      <div className="mp-container-right">
-        <div>
-          <h1>선호챔피언</h1>
-        </div>
-        <div>
-          <h1>선호포지션</h1>
-        </div>
+    <div className="mp-container-right">
+      <div>
+        <h1>선호챔피언</h1>
+        <input
+          value={input}
+          onChange={handleInput}
+          placeholder="챔피언을 입력하시오."
+        />
+        <button onClick={OnClickButton}>추가</button>
+        <ul>{champList}</ul>
+      </div>
+      <div>
+        <h1>선호포지션</h1>
       </div>
     </div>
   );
