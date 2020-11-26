66bd6e0e67c5f52b0333ee96699a6e6bb373a063
Jungwon Lee <leejungwon96@gmail.com>
2020-11-26 13:18:44 +0900

    static data추가

--- src/components/mypage/MpRight.js
+++ src/components/mypage/MpRight.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "../css/MyPage.css";
+import champimage from "../../images/Lux.png";
+import champData from '../../static-data/champ-static-data';
 
 const MpRight = () => {
   const [input, setInput] = useState("");
@@ -20,21 +22,28 @@ const MpRight = () => {
     setChamp(nextChamp);
     setInput("");
   };
-  const champList = champ.map((champ) => <li key={champ.id}>{champ.name}</li>);
+  const champList = champ.map((champ) => (
+    <li key={champ.id}>
+      {champ.name}
+      <img className="lux" alt="" src={champimage} />
+    </li>
+  ));
 
   return (
     <div className="mp-container-right">
-      <div>
-        <h1>선호챔피언</h1>
-        <form className="right-form" onSubmit={OnClickButton}>
-          <input
-            value={input}
-            onChange={handleInput}
-            placeholder="챔피언을 입력하시오."
-          />
-          <button type="submit">추가</button>
-          <ul>{champList}</ul>
-        </form>
+      <div className="favchamp-info">
+        <div>
+          <h1>선호챔피언</h1>
+          <form className="right-form" onSubmit={OnClickButton}>
+            <input
+              value={input}
+              onChange={handleInput}
+              placeholder="챔피언을 입력하시오."
+            />
+            <button type="submit">추가</button>
+            <ul>{champList}</ul>
+          </form>
+        </div>
       </div>
       <div className="">
         <h1>선호포지션</h1>
