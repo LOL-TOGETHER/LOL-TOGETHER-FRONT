12caeed000179144952775d6d19d1ef6cbc95cb1
Jungwon Lee <leejungwon96@gmail.com>
2020-11-29 19:07:18 +0900

    css modify

--- src/components/pages/Board.js
+++ src/components/pages/Board.js
@@ -1,31 +1,34 @@
 import React from "react";
 import posts from "../../static-data/tabledata";
+import "../css/Table.css";
 
 const Board = () => {
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>태그</th>
-            <th>작성자</th>
-            <th>글제목</th>
-            <th>작성시간</th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map((post) => {
-            return (
-              <tr>
-                <td>{post.tag}</td>
-                <td>{post.username}</td>
-                <td>{post.content}</td>
-                <td>{post.updatedTime}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>태그</th>
+              <th>작성자</th>
+              <th>글제목</th>
+              <th>작성시간</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post) => {
+              return (
+                <tr>
+                  <td>{post.tag}</td>
+                  <td>{post.username}</td>
+                  <td>{post.content}</td>
+                  <td>{post.updatedTime}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
