d3d53bc06368538a34f3d6db5d695a09fdc2fb34
Jungwon Lee <leejungwon96@gmail.com>
2020-12-03 10:53:49 +0900

    배열뒤집기

--- src/components/pages/Board.js
+++ src/components/pages/Board.js
@@ -12,6 +12,7 @@ const Board = () => {
       setPosts(response.data);
     });
   }, []);
+
   return (
     <div>
       <div className="table-container">
@@ -33,22 +34,25 @@ const Board = () => {
             </tr>
           </thead>
           <tbody>
-            {posts.map((post) => {
-              return (
-                <tr>
-                  <td className="c0">{post.line}</td>
-                  <td className="c1">
-                    <Link to="/summonerinfo">{post.userName}</Link>
-                  </td>
+            {posts
+              .slice(0)
+              .reverse()
+              .map((post) => {
+                return (
+                  <tr>
+                    <td className="c0">{post.line}</td>
+                    <td className="c1">
+                      <Link to="/summonerinfo">{post.userName}</Link>
+                    </td>
 
-                  <td className="c2">
-                    <Link to={`/detailpage/${post.id}`}>{post.title}</Link>
-                  </td>
+                    <td className="c2">
+                      <Link to={`/detailpage/${post.id}`}>{post.title}</Link>
+                    </td>
 
-                  <td className="c3">{formatdate(post.updated_date_time)}</td>
-                </tr>
-              );
-            })}
+                    <td className="c3">{formatdate(post.updated_date_time)}</td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
         <div className="pgntion">PAGINATION HERE</div>
