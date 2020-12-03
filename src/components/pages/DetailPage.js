2cc989f9a5c8daf506387ceafc795832ae3646bf
Jungwon Lee <leejungwon96@gmail.com>
2020-12-03 15:22:48 +0900

    comment get 해서 조회만 구현하면 댓글 끝

--- src/components/pages/DetailPage.js
+++ src/components/pages/DetailPage.js
@@ -4,7 +4,39 @@ import "../css/DetailPage.css";
 
 const DetailPage = () => {
   const [post, setPost] = useState("");
+  const [comments, setComments] = useState("");
+  const [commentuser, setCommentuser] = useState("");
 
+  const handleCommentContent = (e) => {
+    setComments(e.target.value);
+  };
+  const handleCommentuser = (e) => {
+    setCommentuser(e.target.value);
+  };
+
+  const deleteComment = (commentId) => {
+    axios.delete(
+      `http://localhost:7000/board/comment?board_Id=${postId}&id=${commentId}`
+    );
+  };
+
+  const commentlist = comments.localeCompare((comment) => {
+    <li key={comment.id}>
+      <div>
+        {comment.userName}:{comment.content}
+      </div>
+      <button
+        type="button"
+        onClick={() => {
+          deleteComment(comment.id);
+        }}
+      >
+        x
+      </button>
+    </li>;
+  });
+
+  const postId = window.location.href.split("detailpage/")[1];
   useEffect(() => {
     const postId = window.location.href.split("detailpage/")[1];
 
@@ -15,6 +47,15 @@ const DetailPage = () => {
       });
   }, []);
   console.log(post);
+
+  const onClickComment = () => {
+    axios
+      .post(`http://localhost:7000/board/comment?boardId=${postId}`, {
+        userName: commentuser,
+        content: comments,
+      })
+      .then((response) => alert(response.message));
+  };
 
   return (
     <div>
@@ -32,19 +73,25 @@ const DetailPage = () => {
             <div className="comment_container">
               <h4> 댓글 </h4>
               <div className="comment_write">
+                <input
+                  value={commentuser}
+                  onChange={handleCommentuser}
+                  placeholder="닉네임(인증구현되면지움)"
+                />
                 <textarea
                   rows="3"
                   maxLength="100"
+                  value={comments}
+                  onChange={handleCommentContent}
                   placeholder="댓글을 입력하시오."
                 />
-                <button type="button">등록</button>
+                <button type="button" onClick={onClickComment}>
+                  등록
+                </button>
               </div>
             </div>
-            <ul>
-              <li>페이커: 와 ㅋㅋㅋ 대박인데</li>
-              <li>쇼메이커: 어그로 실화야?</li>
-              <li>뱅: 노잼. 글삭하셈</li>
-            </ul>
+
+            <ul>{commentlist}</ul>
           </div>
         </div>
       </div>
