dc8862d6c3151706138d191ae6f4bff9d1e240ec
Jungwon Lee <leejungwon96@gmail.com>
2020-12-04 00:51:32 +0900

    로그인 구현 성공한거같음!

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const HandleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const HandlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const onClickLogIn = () => {
+    axios
+      .post(`http://13.209.193.142:7000/signup`, {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        localStorage.setItem("token", response.data);
+        history.push("/board");
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+      });
+  };
+
   return (
     <div className="login-container-bottom">
       <form className="login">
@@ -9,6 +35,8 @@ const LoginForm = () => {
           <input
             type="email"
             name="email"
+            onChange={HandleEmail}
+            value={email}
             className="login-input"
             placeholder="이메일을 입력하시오"
           />
@@ -18,11 +46,17 @@ const LoginForm = () => {
           <input
             type="password"
             name="password"
+            value={password}
+            onChange={HandlePassword}
             className="login-input"
             placeholder="비밀번호를 입력하시오"
           />
         </div>
-        <button className="login-input-btn" type="submit">
+        <button
+          onClick={onClickLogIn}
+          className="login-input-btn"
+          type="submit"
+        >
           로그인
         </button>
       </form>
