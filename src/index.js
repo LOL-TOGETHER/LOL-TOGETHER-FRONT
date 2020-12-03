74619fbfc2926ac26cf09d71d0c0de2d4ec03e5e
Jungwon Lee <leejungwon96@gmail.com>
2020-12-03 16:02:08 +0900

    dsfds

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <App />
   </Router>,
   document.getElementById("root")
